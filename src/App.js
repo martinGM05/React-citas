daaec773056695ec6e5fd4817a685a85fc30a4ef
martinGM05 <mtn.gm05@gmail.com>
2021-08-02 23:31:55 -0500

    PropTypes added

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, {Fragment, useState, useEffect} from 'react';
+import Formulario from './components/Formulario';
+import Cita from './components/Cita';
+
+function App() {
+
+  // Citas en local storage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  
+  if(!citasIniciales) {
+    citasIniciales = [];
+  }
+
+  //Arreglo de citas
+  const [citas, guardarCitas] = useState(citasIniciales);
+
+  // Use Effect para realizar ciertas operaciones cuando el state cambia
+  useEffect(() => {
+    if(citasIniciales){
+      localStorage.setItem('citas', JSON.stringify(citas));
+    }else{
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas, citasIniciales]);
+
+  // Función que tome las citas actuales y arregle la nueva
+  const crearCita = cita => {
+    guardarCitas([...citas,cita]);
+  }
+
+  //Funcion que elimina una cita por su id
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(c => c.id !== id);
+    guardarCitas(nuevasCitas);
+  }
+
+  // Mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas';
+   
+  return (
+    <Fragment>
+      <h1>Administrador de pacientes</h1>
+
+    <div className='container'>
+      <div className='row'>
+        <div className='one-half column'>
+            <Formulario 
+              crearCita={crearCita}
+            />
+        </div>
+        <div className='one-half column'>
+            <h2>{titulo}</h2>
+            {citas.map(cita =>(
+              <Cita
+              key={cita.id}
+              cita={cita}
+              eliminarCita={eliminarCita}
+              />
+            ))}
+        </div>
+      </div>
+    </div>
+    </Fragment>
+  );
+}
+
+export default App;
